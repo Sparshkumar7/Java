e16a547df7f2ad34a7cb43ad0c39d083dc86e91c
Sparsh Kumar <Sparshkumar002@gmail.com>
2025-04-06 16:54:35 +0530

    java

---
+++ src/random.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+public class random {
+    public static void main(String[] args ){
+    Random num= new Random();
+    int num1 = num.nextInt(1, 7);
+        System.out.println(num1);
+    }
+}
