fe87b6473e9ee02cacf382e5ba1713e10f7c1871
Sparsh Kumar <Sparshkumar002@gmail.com>
2025-04-08 21:53:52 +0530

    java

---
+++ src/Practices1.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+public class Practices1 {
+    public static void main(String[] args){
+        //? Q1
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter any Number: ");
+//        int user = input.nextInt();
+//
+//        System.out.println("Print the Integer value: "+ user);
+
+        //! Q2
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter first number: ");
+//        int a = input.nextInt();
+//        System.out.print("Enter second number: ");
+//        int b = input.nextInt();
+//        int sum = a + b;
+//        System.out.println("Sum of the two number: "+ sum);
+
+        //* Q3
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter First float number: ");
+//        float firstfloat = input.nextFloat();
+//        System.out.print("Enter Second float number: ");
+//        float secondfloat = input.nextFloat();
+//        float Multiply = firstfloat * secondfloat;
+//        System.out.print("Multiply of two floating number: "+ Multiply);
+
+        //? q4
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter character: ");
+//        char ch = input.next().charAt(0);
+//        int ascii = ch;
+//        System.out.println("ASCII value of"+ch+"is :"+ ascii);
+
+        //! Q5
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter Dividened: ");
+//        int Dividend = input.nextInt();
+//        System.out.print("Enter Divisor: ");
+//        int Divisor = input.nextInt();
+//
+//        int Remainder = Dividend % Divisor;
+//        int Quotient = Dividend / Divisor;
+//        System.out.println("Remainder: "Remainder);
+//        System.out.println("Quotient :"Quotient);
+
+        //* Q6
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Java Program to Swap Two Numbers");
+//        System.out.print("Enter First Value: ");
+//        int First = input.nextInt();
+//        System.out.print("Enter Second Value: ");
+//        int Second = input.nextInt();
+//        int swap = First;
+//        First =Second;
+//        Second = swap;
+//        System.out.println("First value: "+ First);
+//        System.out.println("Second value: "+ Second);
+        //? Q7
+
+    }
+
+}
+
+
+
+
+
