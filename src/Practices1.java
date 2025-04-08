30e2240122f13bea3c5f57b0b05310d5f64d75ea
Sparsh Kumar <Sparshkumar002@gmail.com>
2025-04-09 02:05:54 +0530

    java

--- src/Practices1.java
+++ src/Practices1.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 public class Practices1 {
     public static void main(String[] args){
-        //? Q1
+        //? Q1 Java Program to Print an Integer (Entered by the User)
 //        Scanner input = new Scanner(System.in);
 //        System.out.print("Enter any Number: ");
 //        int user = input.nextInt();
 //
 //        System.out.println("Print the Integer value: "+ user);
 
-        //! Q2
+        //! Q2 Java Program to Add Two Integers
 //        Scanner input = new Scanner(System.in);
 //        System.out.print("Enter first number: ");
 //        int a = input.nextInt();
@@ -17,7 +17,7 @@ public class Practices1 {
 //        int sum = a + b;
 //        System.out.println("Sum of the two number: "+ sum);
 
-        //* Q3
+        //* Q3 Java Program to Multiply two Floating Point Numbers
 //        Scanner input = new Scanner(System.in);
 //        System.out.print("Enter First float number: ");
 //        float firstfloat = input.nextFloat();
@@ -26,14 +26,14 @@ public class Practices1 {
 //        float Multiply = firstfloat * secondfloat;
 //        System.out.print("Multiply of two floating number: "+ Multiply);
 
-        //? q4
+        //? q4 Java Program to Find ASCII Value of a character
 //        Scanner input = new Scanner(System.in);
 //        System.out.print("Enter character: ");
 //        char ch = input.next().charAt(0);
 //        int ascii = ch;
 //        System.out.println("ASCII value of"+ch+"is :"+ ascii);
 
-        //! Q5
+        //! Q5 Java Program to Compute Quotient and Remainder
 //        Scanner input = new Scanner(System.in);
 //        System.out.print("Enter Dividened: ");
 //        int Dividend = input.nextInt();
@@ -45,7 +45,7 @@ public class Practices1 {
 //        System.out.println("Remainder: "Remainder);
 //        System.out.println("Quotient :"Quotient);
 
-        //* Q6
+        //* Q6 Java Program to Swap Two Numbers
 //        Scanner input = new Scanner(System.in);
 //        System.out.println("Java Program to Swap Two Numbers");
 //        System.out.print("Enter First Value: ");
@@ -55,12 +55,104 @@ public class Practices1 {
 //        int swap = First;
 //        First =Second;
 //        Second = swap;
-//        System.out.println("First value: "+ First);
+//        System.out.println(First.getClass().getSimpleName());
 //        System.out.println("Second value: "+ Second);
-        //? Q7
+
+        //? Q7 Java Program to Convert Character to String
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter any Character: ");
+//        char ch = input.next().charAt(0);
+//        String st = String.valueOf(ch);
+//        System.out.println("String: "+ch);
+//        Object a = ch;
+//        System.out.println(a.getClass());
+
+        //! 8. Java Program to convert char type variables to int
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter Character: ");
+//        char user = input.next().charAt(0);
+//        int num = user;
+//        System.out.println("Integer: "+num);
+//        Object a = num;
+//        System.out.println(a.getClass());
+
+        //* 9. Java Program to convert int type variables to char
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter Integer Value: ");
+//        int num = input.nextInt();
+//        char chara = (char) num;
+//        System.out.println("Char: "+ chara);
+//        Object a = chara;
+//        System.out.println(a.getClass());
+
+        //? 10. Java Program to convert long type variables into int
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter Integer Value: ");
+//        long num = input.nextInt();
+//        int inte = (int) num;
+//        System.out.println("Int: "+ inte);
+//        Object a = inte;
+//        Object b = num;
+//        System.out.println(a.getClass());
+//        System.out.println(b.getClass());
+
+        //! 11. Java Program to convert int type variables to long
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter Integer Value: ");
+//        int num = input.nextInt();
+//        long lon = num;
+//        System.out.println("Long: "+ lon);
+//        Object a = num;
+//        Object b = lon;
+//
+//        System.out.println(a.getClass());
+//        System.out.println(b.getClass());
+
+        //! 12. Java Program to convert boolean variables into string
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter True/False: ");
+//        boolean num = input.nextBoolean();
+//        String str = String.valueOf(num);
+//        System.out.println("str: "+ str );
+//        Object a = num;
+//        Object b = str;
+//        System.out.println(a.getClass());
+//        System.out.println(b.getClass());
+        // ? 13. Java Program to convert string type variables into boolean
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter String value: ");
+//        String num = input.nextLine();
+//        boolean bool = Boolean.valueOf(num);
+//        System.out.println("boolean: "+ bool );
+//        Object a = num;
+//        Object b = bool;
+//        System.out.println(a.getClass());
+//        System.out.println(b.getClass());
+
+        //! 14. Java Program to convert string type variables into int
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter String value: ");
+//        String num = input.nextLine();
+//        int integer = Integer.valueOf(num);
+//        System.out.println("convert String into integer: "+ integer );
+//        Object a = num;
+//        Object b = integer;
+//        System.out.println("Original String type: "+a.getClass());
+//        System.out.println("Converted Integer type: "+b.getClass());
+
+//        ? 15. Java Program to convert int type variables to String
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter Integer value: ");
+//        int num = input.nextInt();
+//        String integer = String.valueOf(num);
+//        System.out.println("convert Integer into String: "+ integer );
+//        Object a = num;
+//        Object b = integer;
+//        System.out.println("Original Integer type: "+a.getClass());
+//        System.out.println("Converted String  type: "+b.getClass());
+
 
     }
-
 }
 
 
